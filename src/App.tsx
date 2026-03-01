fb7f68897f41fe63974a226b343359e2cc0f0bd4
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2026-03-01 23:49:49 +0100

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   
   return (
     <div className='container mx-auto p-4'>
-      <h1 className="text-3xl font-bold text-center mb-8">Portal de Noticias</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">Portal de Noticias El Melon 🍈 </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {NewsList.map((noticia) => (
           <NewsPerfil 
--- src/componentes/NewsPerfil.tsx
+++ src/componentes/NewsPerfil.tsx
@@ -8,13 +8,13 @@ interface NewsPerfilProps {
 
 const NewsPerfil = (props: NewsPerfilProps) => {
 
-    return(
+    return (
         <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl ">
-            <img 
-            src={props.image}
-            alt={props.title}
-            className='w-full h-48 object-cover'
-             />
+            <img
+                src={props.image}
+                alt={props.title}
+                className='w-full h-48 object-cover'
+            />
             <div className='p-4'>
                 <span className='text-xs font-semibold text-blue-600 uppercase tracking-wider'>
                     {props.category}
@@ -23,8 +23,25 @@ const NewsPerfil = (props: NewsPerfilProps) => {
                     {props.title}
                 </h2>
                 <p className='text-gray-600 text-sm leading-relaxed'>
-                 {props.text}
+                    {props.text}
                 </p>
+
+                <a
+                    href="#"
+                    onClick={(e) => e.preventDefault()}
+                    className="mt-auto pt-4 border-t border-gray-100 flex items-center justify-between cursor-pointer group"
+                >
+                    <span className="text-blue-600 font-bold text-sm hover:text-blue-800">
+                        Leer noticia completa
+                    </span>
+
+                    {/* Flecha manual con CSS para asegurar que se vea */}
+                    <span className="text-blue-600 font-bold transform group-hover:translate-x-1 transition-transform">
+                        &rarr;
+                    </span>
+                </a>
+
+
 
             </div>
 
